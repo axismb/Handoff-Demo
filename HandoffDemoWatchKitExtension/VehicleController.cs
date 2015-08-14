cd35b389e7c54047371803f205f0551c500bc252
Aziz Batihk <aziz@azizmb.com>
2015-08-13 23:40:46 -0400

    Added VehicleController.

---
+++ HandoffDemoWatchKitExtension/VehicleController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace HandoffDemoWatchKitExtension
+{
+	partial class VehicleController : WatchKit.WKInterfaceController
+	{
+		public VehicleController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}

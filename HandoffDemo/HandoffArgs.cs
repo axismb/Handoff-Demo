95b9649171d7f32334a125fc83d9b90a66c08671
Aziz Batihk <aziz@azizmb.com>
2015-08-13 23:35:21 -0400

    Created HandoffArgs (EventArgs sub-class).

---
+++ HandoffDemo/HandoffArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HandoffDemo
+{
+	public class HandoffArgs : EventArgs
+	{
+		public string VIN { get; set; }
+
+		public HandoffArgs (string vin)
+		{
+			this.VIN = vin;
+		}
+	}
+}
+

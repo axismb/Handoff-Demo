af6883235e7c030b53051af2cf198087697f67b6
Aziz Batihk <aziz@azizmb.com>
2015-08-13 23:51:27 -0400

    UpdateUserActivity on WillActivate & Invalidate it on WillDeactivate.

--- HandoffDemoWatchKitExtension/VehicleController.cs
+++ HandoffDemoWatchKitExtension/VehicleController.cs
@@ -1,5 +1,6 @@
 using Foundation;
 using System;
+using System.Linq;
 using System.CodeDom.Compiler;
 using UIKit;
 
@@ -7,8 +8,33 @@ namespace HandoffDemoWatchKitExtension
 {
 	partial class VehicleController : WatchKit.WKInterfaceController
 	{
+		private Vehicle _Vehicle { get; set; }
 		public VehicleController (IntPtr handle) : base (handle)
 		{
+		}
+
+		public override void Awake (NSObject context)
+		{
+			base.Awake (context);
+			this._Vehicle = Vehicle.GetVehicles ().First (x => x.VIN);
+		}
+
+		public override void WillActivate ()
+		{
+			base.WillActivate ();
+			this.textLbl.SetText (this._Vehicle.ToString () + "\nHandoff Activated.");
+
+			/// Update User Activity to activate handoff, containing vehicle's VIN.
+			NSDictionary userInfo = new NSDictionary ("VIN", this._Vehicle.VIN);
+			this.UpdateUserActivity ("com.azizmb.handoffdemo.viewing", userInfo, null);
+		}
+
+		public override void DidDeactivate ()
+		{
+			base.DidDeactivate ();
+
+			/// Invalidate User Activity to deactive handoff.
+			this.InvalidateUserActivity();
 		}
 	}
 }
